46eef00721af7604eb82666e4bd9681f134d0fcb
Personal <Home@Joses-MBP.attlocal.net>
2021-08-03 22:10:17 -0700

    Fixed form issues and added developer links

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = (uri)
 
+
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///azeporo') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
